44a03165fcddc69978ed73cefd401cc2b8a09a6d
AlfaRiza <malfariza45@gmail.com>
2021-07-22 12:11:53 +0700

    ADD image

--- main.py
+++ main.py
@@ -8,23 +8,24 @@ contents = requests.get(url)
 
 response = bs4.BeautifulSoup(contents.text, 'html.parser')
 
-data = response.findAll('div', attrs={'class': 'D_apq D_eZ M_gF D_fb M_gH'})
+data = response.find('div', attrs={'class': 'D_apq D_eZ M_gF D_fb M_gH'})
 
-for item in data:
-    datas = item.findAll('div', attrs={'class': 'D_jg', 'class': 'D_qq', 'class': 'D_qv'})
-    print(datas)
-    for obj in datas:
-        print('Title: ', obj.find('p', attrs={'class': "D_bN M_aT D_aP M_aC D_bO M_aU D_bR M_aX D_bT M_aZ D_bW M_bc "
+
+datas = data.findAll('div', attrs={'class': 'D_jg', 'class': 'D_qq', 'class': 'D_qv'})
+    # print(datas)
+for obj in datas:
+    print('Title: ', obj.find('p', attrs={'class': "D_bN M_aT D_aP M_aC D_bO M_aU D_bR M_aX D_bT M_aZ D_bW M_bc "
                                                        "D_bZ M_bg D_bK"}).text)
-        print('Harga: ', obj.find('p', attrs={'class': 'D_bN M_aT D_aP M_aC D_bO M_aU D_bR M_aX D_bT M_aZ D_bW M_bc '
+    print('Img: ', obj.find('img', attrs={'class': 'D_bl', 'class': 'D_bi', 'class': 'D_tO'})['src'])
+    print('Harga: ', obj.find('p', attrs={'class': 'D_bN M_aT D_aP M_aC D_bO M_aU D_bR M_aX D_bT M_aZ D_bW M_bc '
                                                        'D_bY M_bf D_bJ'}).text)
-        descs = obj.findAll('p', attrs={'class': 'D_bN M_aT D_aP M_aC D_bO M_aU D_bR M_aX D_bT M_aZ D_bW '
+    descs = obj.findAll('p', attrs={'class': 'D_bN M_aT D_aP M_aC D_bO M_aU D_bR M_aX D_bT M_aZ D_bW '
                                                            'M_bc D_bY M_bf D_bK'})
-        for desc in descs:
-            print('Deskripsi: ', desc.text)
+    for desc in descs:
+        print('Deskripsi: ', desc.text)
         # print('Keterangan: ', obj.find('p', attrs={'class': 'D_bN M_aT D_aP M_aC D_bO M_aU D_bR M_aX D_bT M_aZ D_bW '
         #                                                     'M_bc D_bY M_bf D_bK'}).text)
-        print('likes: ', obj.find('span', attrs={'class': 'D_bN M_aT D_aP M_aC D_bO M_aU D_bR M_aX D_bU M_ba D_bW '
+    print('likes: ', obj.find('span', attrs={'class': 'D_bN M_aT D_aP M_aC D_bO M_aU D_bR M_aX D_bU M_ba D_bW '
                                                           'M_bc D_bY M_bf D_bK'}).text)
-        print('\n')
+    print('\n')
 
